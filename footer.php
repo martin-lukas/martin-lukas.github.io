98d75c6cce5e960904689ca3655676ce5dfe5e49
Martin Lukáš <martinlukas456@gmail.com>
2017-12-17 14:54:01 +0100

    Copy of Vysokanazkousku
    Serves as a base of operation.

---
+++ footer.php
@@ -0,0 +1,3 @@
+<hr>
+Vysoka na zkousku..... | Musime to dodelat všechno... | cojdk jkj | sjflsdkjflsdk  fl.corj .cijdl | dlfjdlskj lk
+<br><br>
---
+++ menu.php
@@ -0,0 +1,4 @@
+<ul>
+	<li><a href="index.php">Úvod</a></li>
+	<li><a href="login_page.php">Vstup do systému</a></li>
+</ul>
