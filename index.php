4aed0a56f0dcbeb885308be16167e28d7d32337b
Martin Lukáš <martinlukas456@gmail.com>
2017-12-19 16:22:49 +0100

    Making it look nice

--- index.php
+++ index.php
@@ -1,30 +1,21 @@
 <!doctype html>
 <html>
-    <head>
-        <title>Úvod | Vysoká na zkoušku</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="styles.css">
-    </head>
-    <body>
-        <div id="container">
-            <div id="header">
-                <?php include("header.php"); ?>
-            </div>
-            <div id="wrapper">
-                <div id="navigation">
-                    <?php include("menu.php"); ?>
-                </div>
-                <div id="content">
-                    <br>    
-                    <h2>Vítejte</h2>
-                    <p>Možná jste na tuto stránku natrefili omylem. Možná jste tu správně. Možná ani nevíte, co pořádně chcete. Nezoufejte. Přijďte si vyzkoušet vysokou, i když na vysoké třeba už i jste. Proč by ne... vždyť je to zadarmo. Můžete si poslechnout přednášky z rozmanitých oblastí přírodních a humanitních věd, vyzkoušet si napsat test na to, co jste právě slyšeli, a konečně poznat pocit selhání po neúspěšné vysokoškolské zkoušce.</p>
-                    <p>Phasellus sem diam, bibendum ut feugiat quis, fermentum a nulla. Donec arcu purus, iaculis et sem et, pharetra euismod arcu. Suspendisse ornare tincidunt diam, non sollicitudin ligula scelerisque vitae. Nulla in egestas mauris, placerat viverra augue. Mauris vehicula orci vel risus accumsan, dignissim consequat libero varius mauris, vel malesuada nisi magna nec tortor.</p>
-                    <p>In at ante dignissim, blandit purus sit amet, tristique enim. Curabitur eu turpis iaculis, vehicula nibh a, sagittis justo. Pellentesque fermentum ornare magna vel porta. Sed felis erat, consequat eget tempus ut, efficitur ut elit. Fusce enim est, imperdiet ac dignissim eu, sodales ac nisi. Proin mattis, urna eget dignissim dapibus, metus libero varius mauris, vel malesuada nisi magna nec tortor.</p>
-                </div>
-            </div>
-            <div id="footer">
-                <?php include("footer.php"); ?>
-            </div>
-        </div>
-    </body>
+<head>
+<title>Osobní blog | Můj web s portfoliem.</title>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<div id="header">
+    <h1>Martin Lukáš</h1>
+</div>
+<div id="container">
+    <div id="content">
+        skdjfalskd kdjfal jfdslk jdlskfj dksfjalds ksdjlf kjasdl fksdlk ajdslf ksdl kjds lakdj flasdk jfalsdk jsd lkjdsaflskdf jlskdj laskd jsladk fjslkd fjsdl kjsd asdlfskdfj kladsj ldks jdlsk jsdl fkjsdl kjsd lfkasjd lkdsj lksad jflsdkj lsad jkadsl kjdslf kjdsl akdsjf lsdkj sld kjdsl kjfsdal skdjl k
+    </div>
+</div>
+<div id="footer">
+    &copy; Martin Lukáš, 2017
+</div>
+</body>
 </html>
